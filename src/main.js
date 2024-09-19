60f4304d3c0fdd70c8d98de2cd7ac9893bf5dd3b
gp-kobayashi <kobayashi_y@gp1000.jp>
2024-09-19 13:44:20 +0900

    [fix]タイプミスの修正

--- src/main.js
+++ src/main.js
@@ -2,22 +2,22 @@ class Block {
     constructor(x, y){
         this.x = x;
         this.y = y;
-    };
+    }
     draw(){
-        Push();
-        let s = 25;
-        Reflect(s*this.x, s*this.y, s, s);
+        push();
+        let s = width/12;
+        rect(s*this.x, s*this.y, s, s);
         pop();
     }
-};
+}
 
 class Mino {
     constructor(x, y, rot, shape){
         this.x = x;
         this.y = y;
         this.rot = rot;
-        this.shape = shape
-    };
+        this.shape = shape;
+    }
     calcBlocks(){
         let blocks= [];
          switch(this.shape) {
@@ -28,17 +28,16 @@ class Mino {
            case 4: blocks = [new Block(0,-2),new Block(0,-1),new Block(-1,0),new Block(0,0)];break;
            case 5: blocks = [new Block(-1,-1),new Block(-1,0),new Block(0,0),new Block(0,-1)];break;
            case 6: blocks = [new Block(-2,0),new Block(-1,0),new Block(0,0),new Block(1,0)];break;
-            };
+            }
         let rot = (40000000 + this.rot) % 4;
         for(let r=0; r<rot; r++){
             blocks = blocks.map(b => new Block(-b.y, b.x));
         }
-        blocks.forEach(b => (b.x += this.x, b.y += this.y));
+        blocks = blocks.map(b => new Block(b.x+this.x, b.y+this.y));
         return blocks;
     }
     draw() {
         let blocks = this.calcBlocks();
-
         for(let b of blocks){
             b.draw();
         }
@@ -46,7 +45,7 @@ class Mino {
     copy(){
         return new Mino(this.x, this.y, this.rot, this.shape);
     }
-};
+}
 
 class Field {
     constructor(){
@@ -73,38 +72,46 @@ class Field {
             [1,0,0,0,0,0,0,0,0,0,0,1],
             [1,1,1,1,1,1,1,1,1,1,1,1],
         ];
-    };
+    }
     tileAt(x, y) {
+        if (x<0 || x>=12 || y<0 || y>=21) return 1;
         return this.tiles[y][x];
     }
     putBlock(x, y){
         this.tiles[y][x]= 1;
     }
     findLineFilled(){
-        for(let y=0; y<21; y++){
+        for(let y=0; y<20; y++){
             let isFilled = this.tiles[y].every(t => t===1);
             if(isFilled) return y;
         }
+        return -1;
+    }
+    cutLine(y) {
+        this.tiles.splice(y, 1);
+        this.tiles.unshift([1,0,0,0,0,0,0,0,0,0,0,1]);
     }
     draw() {
-        for(let y=0; y<21; y++){
-            for(let x=0; x=12; x++){
-                if (this.tileAt(x, y) === 0) continue;
-                new Block(x, y).draw();
-            }
+        for(let y=0; y<21; y++) {
+          for(let x=0; x<12; x++) {
+            if (this.tileAt(x,y) === 0) continue;
+            new Block(x,y).draw();
+          }
         }
     }
-};
+}
 
 class Game {
-    constructor(){
+    constructor() {
         this.mino = Game.makeMino();
         this.minoVx = 0;
+        this.minoDrop = false;
+        this.minoVr = 0;
         this.field = new Field();
         this.fc = 0;
     }
     static makeMino(){
-        return new Mino(5, 10, 0, floor(random(0,7)));
+        return new Mino(5, 2, 0, floor(random(0,7)));
     }
     static isMinoMovable(mino, field){
         let blocks = mino.calcBlocks();
@@ -112,25 +119,30 @@ class Game {
     }
     proc(){
         //落下
-        if(this.fc % 20 === 19){
+        if (this.minoDrop || (this.fc%20) === 19) {
             let futureMino = this.mino.copy();
             futureMino.y += 1;
-            if(Game.isMinoMovable(futureMino, this.filed)){
-                this.mino.y += 1;
+            if (Game.isMinoMovable(futureMino, this.field)) {
+              this.mino.y += 1;
             }else{
                 //接地
                 for(let b of this.mino.calcBlocks()){
                   this.field.putBlock(b.x, b.y);
                   this.mino = Game.makeMino();
                 }
-                
             }
+        // 消去 Erase lines.
+        let line;
+            while((line = this.field.findLineFilled()) !== -1) {
+            this.field.cutLine(line);
+            }
+        this.minoDrop = false;
         }
         //左右移動
         if(this.minoVx !== 0){
             let futureMino = this.mino.copy();
             futureMino.x += this.minoVx;
-            if(Game.isMinoMovable(futureMino, this.filed)){
+            if(Game.isMinoMovable(futureMino, this.field)){
                 this.mino.x += this.minoVx;
             }
             this.minoVx = 0;
@@ -139,7 +151,7 @@ class Game {
         if(this.minoVr !== 0){
             let futureMino = this.mino.copy();
             futureMino.rot += this.minoVr;
-            if(Game.isMinoMovable(futureMino, this.filed)){
+            if(Game.isMinoMovable(futureMino, this.field)){
                 this.mino.rot += this.minoVr;
             }
             this.minoVr = 0;
@@ -154,16 +166,15 @@ class Game {
 }
 let geme;
 
-function keyPressed(){
-    if(keyCode === 65)Game.minoVx = -1;
-    if(keyCode === 68)Game.minoVx = 1;
-    if(keyCode === 81)Game.minoVr = -1;
-    if(keyCode === 69)Game.minoVr = 1;
-}
-
+function keyPressed() {
+    if (keyCode === 65) game.minoVx = -1;
+    if (keyCode === 68) game.minoVx = 1;
+    if (keyCode === 81) game.minoVr = -1;
+    if (keyCode === 69) game.minoVr = 1;
+    if (keyCode === 83) game.minoDrop = true;
+  }
 function setup() {
-    createCanvas(400, 600);
-    background(64);
+    createCanvas(360, 720);
     game = new Game();
 }
 
